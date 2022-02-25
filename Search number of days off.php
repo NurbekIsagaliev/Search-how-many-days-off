8326b1f505c5b06eff929ba6cda68c367b1ba9aa
NurbekIsagaliev <71828298+NurbekIsagaliev@users.noreply.github.com>
2022-02-25 17:49:27 +0500

    Update Search number of days off.php

--- Search number of days off.php
+++ Search number of days off.php
@@ -2,6 +2,9 @@
 /*Функия Weekend которая  возвращает целое число.Необходимо было написать функцию которая принимает два 
 параметра типа строки(две даты),и вычисляет сколько выходных дней между этими датами,если даты равны то 
 вычисляется  эта дата (выходной или нет).Выходные дни-суббота и воскресенье.
+The Weekend function that returns an integer. It was necessary to write a function that takes two
+string type parameter (two dates), and calculates how many days off between these dates, if the dates are equal then
+this date is calculated (weekend or not). Weekend days are Saturday and Sunday
 */
 function Weekend(string $begin, string $end):int {
 
